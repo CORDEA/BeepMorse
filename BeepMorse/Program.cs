20f2e7b672fa461ef152d8b82d3c516360f6684c
Yoshihiro Tanaka <contact@cordea.jp>
2022-09-04 22:34:49 +0900

    sound morse code

--- BeepMorse/Program.cs
+++ BeepMorse/Program.cs
@@ -1,2 +1,18 @@
 ﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+
+const int frequency = 800;
+const int duration = 100;
+
+const string query = "hello world";
+var morse = new Morse.Morse(query).Generate();
+
+foreach (var m in morse)
+{
+    if (m >= 0)
+    {
+        Console.Beep(frequency, m == 0 ? duration : duration * 3);
+        continue;
+    }
+
+    Thread.Sleep(duration);
+}
