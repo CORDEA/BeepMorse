af8bf6ff6dbbe56f5cb0c303e301380bc88f9d67
Yoshihiro Tanaka <contact@cordea.jp>
2022-09-04 15:40:33 +0900

    generate the code

---
+++ Morse/Morse.cs
@@ -0,0 +1,59 @@
+﻿namespace Morse
+{
+    public class Morse
+    {
+        private static readonly int[][] Table =
+        {
+            new[] {0, 1},
+            new[] {1, 0, 0, 0},
+            new[] {1, 0, 1, 0},
+            new[] {1, 0, 0},
+            new[] {0},
+            new[] {0, 0, 1, 0},
+            new[] {1, 1, 0},
+            new[] {0, 0, 0, 0},
+            new[] {0, 0},
+            new[] {0, 1, 1, 1},
+            new[] {1, 0, 1},
+            new[] {0, 1, 0, 0},
+            new[] {1, 1},
+            new[] {1, 0},
+            new[] {1, 1, 1},
+            new[] {0, 1, 1, 0},
+            new[] {1, 1, 0, 1},
+            new[] {0, 1, 0},
+            new[] {0, 0, 0},
+            new[] {1},
+            new[] {0, 0, 1},
+            new[] {0, 0, 0, 1},
+            new[] {0, 1, 1},
+            new[] {1, 0, 0, 1},
+            new[] {1, 0, 1, 1},
+            new[] {1, 1, 0, 0},
+            new[] {0, 1, 1, 1, 1},
+            new[] {0, 0, 1, 1, 1},
+            new[] {0, 0, 0, 1, 1},
+            new[] {0, 0, 0, 0, 1},
+            new[] {0, 0, 0, 0, 0},
+            new[] {1, 0, 0, 0, 0},
+            new[] {1, 1, 0, 0, 0},
+            new[] {1, 1, 1, 0, 0},
+            new[] {1, 1, 1, 1, 0},
+            new[] {1, 1, 1, 1, 1},
+        };
+
+        private const string Code = "abcdefghijklmnopqrstuvwxyz1234567890";
+
+        private readonly string _query;
+
+        public Morse(string query)
+        {
+            _query = query;
+        }
+
+        public IEnumerable<int> Generate()
+        {
+            return _query.ToCharArray().SelectMany(e => Table[Code.IndexOf(e)]);
+        }
+    }
+}
